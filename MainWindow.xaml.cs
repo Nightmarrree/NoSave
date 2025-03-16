a797b559c3c788a146b51fd666b6db4e688b487c
レイちゃん <143278838+Nightmarrree@users.noreply.github.com>
2025-03-16 22:37:35 +0100

    feat: add initial project files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace NoSave
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _ = Task.Run(StatusChecker);
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (Utilities.CheckFirewallRule())
+            {
+                Utilities.SetFirewall(false);
+            }
+            else
+            {
+                Utilities.SetFirewall(true);
+            }
+        }
+
+        private async Task StatusChecker()
+        {
+            App app = Application.Current as App;
+
+            while (true)
+            {
+                bool ruleExists = Utilities.CheckFirewallRule();
+
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    if (ruleExists)
+                    {
+                        Button.Content = "Unblock R*";
+                    }
+                    else
+                    {
+                        Button.Content = "Block R*";
+                    }
+                });
+
+                await Task.Delay(200);
+            }
+        }
+
+
+        private void DragWindow(object sender, RoutedEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace NoSave
+{
+    public static class Utilities
+    {
+        public static bool IsFirewallEnabled()
+        {
+            string output = Cmd("netsh advfirewall show allprofiles state");
+            return !output.Contains("OFF");
+        }
+
+        public static void SetFirewall(bool state)
+        {
+            if (CheckFirewallRule() != state)
+            {
+                if (state)
+                {
+                    EnableNoSavingMode();
+                }
+                else
+                {
+                    DisableNoSavingMode();
+                }
+            }
+        }
+
+        public static void EnableNoSavingMode()
+        {
+            App app = Application.Current as App;
+            string output = Cmd($"netsh advfirewall firewall add rule name=\"{app.ruleName}\" dir=out action=block remoteip={app.remoteIP} enable=yes");
+        }
+
+        public static void DisableNoSavingMode()
+        {
+            App app = Application.Current as App;
+            string output = Cmd($"netsh advfirewall firewall delete rule name=\"{app.ruleName}\"");
+        }
+
+        public static bool CheckFirewallRule()
+        {
+            App app = Application.Current as App;
+            string output = Cmd($"netsh advfirewall firewall show rule name=\"{app.ruleName}\"");
+            return output.Contains(app.ruleName);
+        }
+
+        public static string Cmd(string line)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C {line}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(psi))
+            {
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                return output;
+            }
+        }
+    }
+}
