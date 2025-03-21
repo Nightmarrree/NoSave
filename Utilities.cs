ad5b9f07c501dc293d3ae22d14023ac588bbffed
レイちゃん <143278838+Nightmarrree@users.noreply.github.com>
2025-03-21 08:32:37 +0100

    feat: add global hotkey support

--- Utilities.cs
+++ Utilities.cs
@@ -1,29 +1,52 @@
 ﻿using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace NoSave
 {
     public static class Utilities
     {
+        public static void SwitchFirewallRule()
+        {
+            SetFirewall(!CheckFirewallRule());
+        }
+
+        public static void SetFirewall(bool state)
+        {
+            if (CheckFirewallRule() == state)
+                return;
+
+            if (state) 
+                EnableNoSavingMode(); 
+            else 
+                DisableNoSavingMode();
+        }
+
+        public static async Task StatusChecker(Button button)
+        {
+            while (true)
+            {
+                bool ruleExists = CheckFirewallRule();
+
+                await button.Dispatcher.InvokeAsync(() =>
+                    button.Content = ruleExists ? "Unblock R*" : "Block R*"
+                );
+
+                await Task.Delay(200);
+            }
+        }
+
         public static bool IsFirewallEnabled()
         {
             string output = Cmd("powershell -Command \"(Get-NetFirewallProfile).Enabled\"");
             return !output.Contains("False");
         }
-
-        public static void SetFirewall(bool state)
+        public static bool CheckFirewallRule()
         {
-            if (CheckFirewallRule() != state)
-            {
-                if (state)
-                {
-                    EnableNoSavingMode();
-                }
-                else
-                {
-                    DisableNoSavingMode();
-                }
-            }
+            App app = Application.Current as App;
+            string output = Cmd($"netsh advfirewall firewall show rule name=\"{app.ruleName}\"");
+            return output.Contains(app.ruleName);
         }
 
         public static void EnableNoSavingMode()
@@ -36,13 +59,6 @@ namespace NoSave
         {
             App app = Application.Current as App;
             string output = Cmd($"netsh advfirewall firewall delete rule name=\"{app.ruleName}\"");
-        }
-
-        public static bool CheckFirewallRule()
-        {
-            App app = Application.Current as App;
-            string output = Cmd($"netsh advfirewall firewall show rule name=\"{app.ruleName}\"");
-            return output.Contains(app.ruleName);
         }
 
         public static string Cmd(string line)
